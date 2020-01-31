0f3fc60eaadd78f256e7c207fdfdf66928380de4
eloi.grau <marchelibre@gmx.fr>
2020-01-31 13:39:32 +0100

    init

--- blog/models.py
+++ blog/models.py
@@ -86,7 +86,7 @@ def on_save_articles(instance, created, **kwargs):
         message = " Un nouvel article a été créé " + \
                   "\n Vous pouvez y accéder en suivant ce lien : https://pacteacvi.herokuapp.com" + instance.get_absolute_url() + \
                   "\n\n------------------------------------------------------------------------------" \
-                  "\n vous recevez cet email, car vous avez choisi de suivre les articles (en cliquant sur la cloche) sur le site http://www.Perma.Cat/forum/articles/"
+                  "\n vous recevez cet email, car vous avez choisi de suivre les articles (en cliquant sur la cloche) sur le site https://pacteacvi.herokuapp.com/forum/articles/"
         emails = [suiv.email for suiv in followers(suivi) if instance.auteur != suiv and (instance.estPublic or suiv.is_membre_collectif)]
         try:
             send_mass_mail([(titre, message, "pacteacvi@gmail.com", emails), ])
@@ -117,7 +117,7 @@ def on_save_article(instance, **kwargs):
     message = "L'article '" +  instance.titre + "' a été modifié (ou quelqu'un l'a commenté)" +\
               "\n Vous pouvez y accéder en suivant ce lien : http://pacteacvi.herokuapp.com" + instance.get_absolute_url() + \
               "\n\n------------------------------------------------------------------------------" \
-              "\n vous recevez cet email, car vous avez choisi de suivre ce projet sur le site http://www.Perma.Cat/forum/articles/"
+              "\n vous recevez cet email, car vous avez choisi de suivre ce projet sur le site https://pacteacvi.herokuapp.com/forum/articles/"
    # emails = [(titre, message, "asso@perma.cat", (suiv.email, )) for suiv in followers(instance)]
     emails = [suiv.email for suiv in followers(instance)  if instance.auteur != suiv  and (instance.estPublic or suiv.is_membre_collectif)]
     try:
